0e181ff6e372c67690990ae5a9c1410603edb08f
eahb0y <getelementby111@gmail.com>
2023-02-17 00:05:00 +0500

    created reusable cart into components directory

---
+++ lib/components/reusable_card.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+  final Widget cardChild;
+  final void Function()? onTab;
+  const ReusableCard({
+    Key? key,
+    required this.color,
+    required this.cardChild,
+    this.onTab,
+  },
+) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTab,
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: cardChild,
+      ),
+    );
+  }
+}
